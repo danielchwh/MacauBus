521a980964b36e5a1f39ae7c27a62b7ee4d988d7
danielchwh <55081215+danielchwh@users.noreply.github.com>
2020-06-19 18:34:29 +0800

    Set visibility

--- app/src/main/java/com/danielchwh/macaubus/RouteAdapter.java
+++ app/src/main/java/com/danielchwh/macaubus/RouteAdapter.java
@@ -3,9 +3,11 @@ package com.danielchwh.macaubus;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
@@ -30,6 +32,13 @@ public class RouteAdapter extends RecyclerView.Adapter<RouteAdapter.ContentsView
         RouteInfo station = routeInfo.get(position);
         holder.stationName.setText(station.staName);
         holder.stationCode.setText(station.staCode);
+        if (station.busInfo == null) {
+            holder.busIcon1.setVisibility(View.INVISIBLE);
+            holder.busPlate1.setVisibility(View.INVISIBLE);
+            holder.busIcon2.setVisibility(View.GONE);
+            holder.busPlate2.setVisibility(View.GONE);
+            holder.drivingCard.setVisibility(View.GONE);
+        }
     }
 
     @Override
@@ -39,11 +48,19 @@ public class RouteAdapter extends RecyclerView.Adapter<RouteAdapter.ContentsView
 
     static class ContentsViewHolder extends RecyclerView.ViewHolder {
         public TextView stationName, stationCode;
+        public ImageView busIcon1, busIcon2;
+        public TextView busPlate1, busPlate2;
+        public CardView drivingCard;
 
         public ContentsViewHolder(@NonNull View itemView) {
             super(itemView);
             stationName = itemView.findViewById(R.id.stationName);
             stationCode = itemView.findViewById(R.id.stationCode);
+            busIcon1 = itemView.findViewById(R.id.busIcon1);
+            busIcon2 = itemView.findViewById(R.id.busIcon2);
+            busPlate1 = itemView.findViewById(R.id.busPlate1);
+            busPlate2 = itemView.findViewById(R.id.busPlate2);
+            drivingCard = itemView.findViewById(R.id.drivingCard);
         }
     }
 }
