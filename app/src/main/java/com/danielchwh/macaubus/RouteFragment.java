98ce6ee84b7a43ab734ccc1bac962eb00e83d680
danielchwh <danielchwh@gmail.com>
2020-06-20 17:59:27 +0800

    Fix bug

--- app/src/main/java/com/danielchwh/macaubus/RouteFragment.java
+++ app/src/main/java/com/danielchwh/macaubus/RouteFragment.java
@@ -1,17 +1,16 @@
 package com.danielchwh.macaubus;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Parcelable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -31,6 +30,8 @@ import java.util.List;
 public class RouteFragment extends Fragment {
     RecyclerView recyclerView;
     FloatingActionButton floatingActionButton;
+    TextView failureMsg;
+    RequestQueue queue;
     RouteAdapter adapter;
     String route;
     private static final int REFRESH_INTERVAL = 5000;
@@ -46,14 +47,21 @@ public class RouteFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_route, container, false);
         recyclerView = view.findViewById(R.id.recyclerView_Route);
         floatingActionButton = view.findViewById(R.id.floatingActionButton_Route);
+        failureMsg = view.findViewById(R.id.failureMsg_Route);
         recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
+        queue = Volley.newRequestQueue(requireContext());
         route = getArguments().getString("route");
-        initialize();
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("路線 " + route);
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initialize();
     }
 
     @Override
@@ -61,6 +69,8 @@ public class RouteFragment extends Fragment {
         super.onPause();
         if (refreshHandler != null && refreshRunnable != null)
             refreshHandler.removeCallbacks(refreshRunnable);
+        if (queue != null)
+            queue.stop();
     }
 
     @Override
@@ -68,6 +78,15 @@ public class RouteFragment extends Fragment {
         super.onResume();
         if (refreshRunnable != null)
             refreshRunnable.run();
+        if (queue != null)
+            queue.start();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (queue != null)
+            queue.cancelAll(this);
     }
 
     @Override
@@ -88,7 +107,6 @@ public class RouteFragment extends Fragment {
 
     private void initialize() {
         String url = "https://bis.dsat.gov.mo:37812/macauweb/getRouteData.html?action=sd&routeName=" + route + "&dir=0&lang=zh-tw";
-        RequestQueue queue = Volley.newRequestQueue(requireContext());
         StringRequest request = new StringRequest(
                 StringRequest.Method.GET,
                 url,
@@ -101,7 +119,7 @@ public class RouteFragment extends Fragment {
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-
+                        failureMsg.setVisibility(View.VISIBLE);
                     }
                 }
         );
@@ -114,8 +132,10 @@ public class RouteFragment extends Fragment {
         List<RouteInfo> routeInfo = data.routeInfo;
         if (data.direction < 2)
             floatingActionButton.show();
-        if (routeInfo == null)
+        if (routeInfo == null) {
+            failureMsg.setVisibility(View.VISIBLE);
             return;
+        }
         for (int i = 0; i < routeInfo.size(); i++) {
             RouteInfo station = routeInfo.get(i);
             myRouteInfo.add(new MyRouteInfo(station));
@@ -139,7 +159,6 @@ public class RouteFragment extends Fragment {
 
     private void refresh() {
         String url = "https://bis.dsat.gov.mo:37812/macauweb/routestation/bus?action=dy&routeName=" + route + "&dir=0&lang=zh-tw";
-        RequestQueue queue = Volley.newRequestQueue(requireContext());
         StringRequest request = new StringRequest(
                 StringRequest.Method.GET,
                 url,
